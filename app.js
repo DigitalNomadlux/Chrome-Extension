2a8b3e1a93026f6ebf9f2ffb2b75993ad4229b44
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-12-09 22:38:08 -0600

    rendering leads start

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
 let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
-const inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener('click', function() {
-  console.log('send it!')
+  myLeads.push(inputEl.value)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i])
+}
