bfb74ef8f045524e486b60ec3ff76cc7109148cc
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-12-09 22:14:55 -0600

    setting up btn functions

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el")
+
+inputBtn.addEventListener('click', function() {
+  console.log('send it!')
+})
