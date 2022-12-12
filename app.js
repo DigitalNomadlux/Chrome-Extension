2ebf6c6499bad6d4fb2347e864dc2f4b8a379941
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-12-11 18:21:47 -0600

    created list building and render func

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+let listItems = "";
 
 inputBtn.addEventListener('click', function() {
   myLeads.push(inputEl.value)
-})
+  renderLeads()
+});
 
-for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i])
+function renderLeads() {
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>${myLeads[i]}</li>`
+  };
+  ulEl.innerHTML = listItems;
 }
