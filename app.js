86ed90897496ff6edf360a703f121dd86b88fa95
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-12-12 23:01:33 -0600

    clear input value

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
-let listItems = "";
+
 
 inputBtn.addEventListener('click', function() {
   myLeads.push(inputEl.value)
+  inputEl.value = ""
   renderLeads()
 });
 
 function renderLeads() {
+  let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `<li>${myLeads[i]}</li>`
   };
