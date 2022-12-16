bc54e8eeaf9157e0cc08e717a6a7385a6c2ba14c
Austin Turturro <82427116+DigitalNomadlux@users.noreply.github.com>
2022-12-15 22:46:52 -0600

    added delete btn

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ let myLeads = [];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+
+deleteBtn.addEventListener('dblclick', function() {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+})
 
 inputBtn.addEventListener('click', function() {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
   renderLeads();
 });
 
